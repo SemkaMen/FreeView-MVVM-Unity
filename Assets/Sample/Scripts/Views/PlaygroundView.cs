f4d558cbb19dda65e7774a02a5f27f7947a42917
SemkaMen <simonmen87@gmail.com>
2025-01-22 16:30:03 +0100

    Add test card controller and components on sample view

--- Assets/Sample/Scripts/Views/PlaygroundView.cs
+++ Assets/Sample/Scripts/Views/PlaygroundView.cs
@@ -2,15 +2,23 @@ using FreeView.Bindings;
 using FreeView.Views;
 using FreeView.Views.Attributes;
 using Sample.Scripts.ViewModels;
+using TMPro;
+using UnityEngine.UI;
 
 namespace Sample.Scripts.Views
 {
     [ViewPresentation(CanvasContainerName = "MainCanvas")]
     public class PlaygroundView : BaseView<PlaygroundBaseViewModel>
     {
+        private Button _rotateCardButton;
+        private TextMeshProUGUI _scoreText;
+        
         protected override void OnViewAwake()
         {
             base.OnViewAwake();
+
+            _rotateCardButton = GetElementComponent<Button>("_");
+            _scoreText = GetElementComponent<TextMeshProUGUI>("_");
         }
 
         protected override void OnViewStart()
