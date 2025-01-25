809ef8144ee76a64497e66fdfd313c6364a82bd0
SemkaMen <simonmen87@gmail.com>
2025-01-25 14:06:14 +0100

    Renamings, add part of the sample logic

--- Assets/Sample/Scripts/Views/PlaygroundView.cs
+++ Assets/Sample/Scripts/Views/PlaygroundView.cs
@@ -10,15 +10,27 @@ namespace Sample.Scripts.Views
     [ViewPresentation(CanvasContainerName = "MainCanvas")]
     public class PlaygroundView : BaseView<PlaygroundBaseViewModel>
     {
-        private Button _rotateCardButton;
-        private TextMeshProUGUI _scoreText;
+        private Button _toggleDoorButton;
+        private TextMeshProUGUI _counterText;
         
+        private int _counterValue;
+
+        public int CounterValue
+        {
+            get => _counterValue;
+            set
+            {
+                _counterValue = value;
+                _counterText.text = $"Counter: {value}";
+            }
+        }
+
         protected override void OnViewAwake()
         {
             base.OnViewAwake();
 
-            _rotateCardButton = GetElementComponent<Button>("_");
-            _scoreText = GetElementComponent<TextMeshProUGUI>("_");
+            _toggleDoorButton = GetElementComponent<Button>("_toggleDoorButton");
+            _counterText = GetElementComponent<TextMeshProUGUI>("_counterText");
         }
 
         protected override void OnViewStart()
@@ -26,7 +38,25 @@ namespace Sample.Scripts.Views
             base.OnViewStart();
         
             var set = this.CreateBindingSet<PlaygroundView, PlaygroundBaseViewModel>();
+            set.Bind(this).For(v => v.CounterValue).To(vm => vm.CounterValue);
             set.Apply();
+        }
+
+        protected override void OnViewEnable()
+        {
+            base.OnViewEnable();
+            _toggleDoorButton?.onClick.AddListener(OnToggleDoorClicked);
+        }
+
+        protected override void OnViewDisable()
+        {
+            _toggleDoorButton?.onClick.RemoveListener(OnToggleDoorClicked);
+            base.OnViewDisable();
+        }
+
+        private void OnToggleDoorClicked()
+        {
+            ViewModel.ToggleDoor();
         }
     }
 }
